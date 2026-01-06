44278390bc678900213da676d5e1053fd817360f
Sourabh Vamdevan <vamdevansourabh@gmail.com>
2026-01-06 22:21:32 +0530

    added

---
+++ app/lib/mongodb/mongoose.js
@@ -0,0 +1,27 @@
+
+
+
+import mongoose from 'mongoose';
+
+let initialized = false;
+
+export const connect = async () => {
+  mongoose.set('strictQuery', true);
+
+  if (initialized) {
+    console.log('MongoDB already connected');
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'next_estate',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    initialized = true;
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.log('MongoDB connection error:', error);
+  }
+};
